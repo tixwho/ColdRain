e3125c8cd4445e8277cccd6d4f0595b59e53d015
tixwho <905002146tix@gmail.com>
2021-01-16 03:11:31 +0800

    upgrade to jaudiotagger2.2.6 for mp3 md5 functions.

--- src/toolkit/AudioMd5Helper.java
+++ src/toolkit/AudioMd5Helper.java
@@ -1,7 +1,12 @@
 package toolkit;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.flac.FlacAudioHeader;
+import org.jaudiotagger.audio.mp3.MP3File;
 import local.generic.MetaSong;
 
 public class AudioMd5Helper {
@@ -18,6 +23,18 @@ public class AudioMd5Helper {
 
     //regardless of music type, calculate md5 and return.
     public static String getAudioMd5Force(MetaSong meta) {
-        return "";
+        //we only handle mp3 and flac now
+        String md5 = null;
+        AudioFile aof = meta.getAof();
+        if(aof instanceof MP3File) {
+            try {
+                md5= new String(((MP3File)aof).getHash());
+            } catch (NoSuchAlgorithmException | InvalidAudioFrameException | IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            md5=getFlacAudioMd5(meta);
+        }
+        return md5;
     }
 }
