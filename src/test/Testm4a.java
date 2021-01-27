de462b1ce6212484d41e86c36682f7260427a483
tixwho <905002146tix@gmail.com>
2021-01-27 15:40:11 +0800

    Tried m4a support but encountered an unresolved NPE

---
+++ src/test/Testm4a.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.File;
+import exception.MetaIOException;
+import playlist.generic.MetaSong;
+
+public class Testm4a {
+
+    public static void main(String[] args) throws MetaIOException {
+        String loc = "F:\\Discography\\Wonder Egg Priority\\巣立ちの歌\\001-巣立ちの歌.m4a";
+        String loc2 = "F:\\Discography\\Wonder Egg Priority\\Life is サイダー\\001-Life is サイダー.m4a";
+        File nestFile = new File(loc);
+        System.out.println("fileHere:"+nestFile.exists());
+        MetaSong nestMeta = new MetaSong(loc2);
+        System.out.println(nestMeta);
+
+    }
+
+}
