07d5f266e6382c77efcf40c89c5dcc30d9c9ff6f
tixwho <905002146tix@gmail.com>
2021-02-05 21:51:24 +0800

    examined possibility of using cmd to call other scripts.

---
+++ src/scriptrunner/test/TestCmd.java
@@ -0,0 +1,36 @@
+package scriptrunner.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+// copypasta
+public class TestCmd {
+    public static void main(String[] args) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            String cmdStrings[] =
+                {"cmd", "/c", "main.exe", "E:\\lzx\\Discovery\\ColdRain\\Ncm\\水瀬いのり - クリスタライズ.ncm",
+                    "E:\\lzx\\Discovery\\ColdRain\\Ncm\\Luciano Pavarotti - Nessun Dorma.ncm"};
+            String cmdString =
+                "main.exe E:\\\\lzx\\\\Discovery\\\\ColdRain\\\\Ncm\\\\水瀬いのり - クリスタライズ.ncm";
+            String[] envp = {"path=E:\\lzx\\etc\\ncmdump-master"};
+            File dir = new File("E:\\lzx\\etc\\ncmdump-master");
+            Process proc = rt.exec(cmdStrings, envp, dir);
+            // Process proc = rt.exec(cmdString,envp,dir);
+            InputStream stderr = proc.getErrorStream();
+            InputStreamReader isr = new InputStreamReader(stderr);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            System.out.println("<error></error>");
+            while ((line = br.readLine()) != null)
+                System.out.println(line);
+            System.out.println("");
+            int exitVal = proc.waitFor();
+            System.out.println("Process exitValue: " + exitVal);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
--- src/test/TestA.java
+++ src/test/TestA.java
@@ -1,7 +1,36 @@
 package test;
 
 public class TestA extends TestAA{
+    
+    public static int doCommon(int current) {
+        return current -=4;
+    }
+    
+    public static int doUncommon(int current) {
+        return current -=10;
+    }
+    
+    public static int doRare(int current) {
+        return current -=10;
+    }
+    
+    public static int doMythic(int current) {
+        return current -=10;
+    }
+    
     public static void main (String[]args) {
+        int hammer = 1200;//change this
+        
+        int commonCount = 0;
+        int uncommonCount = 0;
+        int rareCount = 0;
+        int mythicCount = 0;
+        
+        do {
+            hammer = doCommon(hammer);
+        }while(hammer >0);
+        
         
     }
+    
 }
